c8903418aa7d97d7fd86d62afb8a6d6fde50104c
MakMax123 <100467867+MakMax123@users.noreply.github.com>
2023-05-06 10:36:36 +0100

    Add files via upload

---
+++ file_system_move_File.py
@@ -0,0 +1,8 @@
+import os
+
+# create source and destination file paths
+source_file_path = 'input your path'
+destination_file_path = 'input your path'
+
+# rename the file using os.rename()
+os.rename(source_file_path, destination_file_path)
